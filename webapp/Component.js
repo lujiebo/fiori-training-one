aa1fe458bbd872bbb8923d052244148de0adce55
unknown <jtlujb@sunnyoptical.com>
2020-05-07 16:58:25 +0800

    修复页面传递参数获取不到的问题

--- webapp/Component.js
+++ webapp/Component.js
@@ -20,11 +20,11 @@ sap.ui.define([
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
 
-			// enable routing
-			this.getRouter().initialize();
-
 			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
+
+			// create the views based on the url/hash
+			this.getRouter().initialize();
 		}
 	});
 });
